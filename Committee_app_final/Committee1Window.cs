4de5aac48bd8f8d8f9b711c89cac87d6fde8251c
adriano davis barrios <nanitodavis@hotmail.com>
2017-06-06 17:04:48 -0500

    minor graphical changes

--- Committee_app_final/Committee1Window.cs
+++ Committee_app_final/Committee1Window.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Graphics;
 
 namespace Committee_app_final
 {
@@ -29,107 +30,169 @@ namespace Committee_app_final
             // Create your application here
             SetContentView(Resource.Layout.Committee1Layout);
             listView = FindViewById<ExpandableListView>(Resource.Id.lvExp);
+
+            //get extra String from the main class
             string text = Intent.GetStringExtra("committee") ?? "Data not available";
+
+            //other view components
             committeeName = FindViewById<TextView>(Resource.Id.textView1);
+            LinearLayout mcl = FindViewById<LinearLayout>(Resource.Id.maincommitteelayout);
 
             //call method populateList to fill the data needed on the dictionary
             optionsMapping = new Dictionary<string, List<string>>();
-            PopulateList();
-
-            listAdapter = new ExpandableListAdapter(this, optionList, optionsMapping);
-            listView.SetAdapter(listAdapter);
 
             //if and el if for asigning the committee title, main image and banner
             if (text.Equals("committee1"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_calidad_circle);
-                committeeName.Text=("Calidad");
+                committeeName.Text=("Comité de Calidad");
+                PopulateList(text);
 
             }
             else if (text.Equals("committee2"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_comunicaciones_circle);
-                committeeName.Text = ("Comunicaciones");
+                committeeName.Text = ("Comité de Comunicaciones");
+                PopulateList(text);
             }
             else if (text.Equals("committee3"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_internacionalizacion_circle);
-                committeeName.Text = ("Internacionalización");
+                committeeName.Text = ("Comité de Internacionalización");
+                PopulateList(text);
             }
             else if (text.Equals("committee4"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_curricular_circle);
-                committeeName.Text = ("Curricular");
+                committeeName.Text = ("Comité Curricular");
+                PopulateList(text);
             }
             else if (text.Equals("committee5"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_rsu_circle);//falta
-                committeeName.Text = ("Responsabilidad Social");
+                committeeName.Text = ("Comité de Responsabilidad Social");
+                PopulateList(text);
             }
             else if (text.Equals("committee6"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_egresados_circle);
-                committeeName.Text = ("Egresados");
+                committeeName.Text = ("Comité de Egresados");
+                PopulateList(text);
             }
             else if (text.Equals("committee7"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_continuada_circle);
-                committeeName.Text = ("Educación Continuada");
+                committeeName.Text = ("Comité de Educación Continuada");
+                PopulateList(text);
             }
             else if (text.Equals("committee8"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_investigacion_circle);
-                committeeName.Text = ("Investigaciones");
+                committeeName.Text = ("Comité de Investigaciones");
+                PopulateList(text);
             }
             else if (text.Equals("committee9"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_externo_circle);
-                committeeName.Text = ("Prácticas Empresariales");
+                committeeName.Text = ("Comité de Prácticas Empresariales");
+                PopulateList(text);
             }
             else if (text.Equals("committee10"))
             {
                 image = FindViewById<ImageView>(Resource.Id.imageView1);
                 image.SetImageResource(Resource.Drawable.icon_exito_circle);
-                committeeName.Text = ("Éxito Estudiantil");
+                committeeName.Text = ("Comité de Éxito Estudiantil");
+                PopulateList(text);
+            }
+            else
+            {
+                PopulateList("default");
             }
 
-            
+            //create expandable list adapter and expandable list view and set them in the layout
+            listAdapter = new ExpandableListAdapter(this, optionList, optionsMapping);
+            listView.SetAdapter(listAdapter);
+            mcl.SetBackgroundColor(Color.ParseColor("#b7df4c"));
+
         }
 
-        public void PopulateList()
+        public void PopulateList(String s)
         {
             //data that will be contained on each option of the expendable list
             //initializing list with options
             optionList = new List<string>();
+            List<string> membersList = new List<string>();
+            List<string> descriptionText = new List<string>();
+            List<string> oldMembersList = new List<string>();
+            List<string> proyectsDone = new List<string>();
+            List<string> proyectsDoing = new List<string>();
+            
+            //fixed headers for the expandable view
             optionList.Add("Descripción");
             optionList.Add("Miembros Activos");
             optionList.Add("Miembros Antiguos");
             optionList.Add("Proyectos Pasados");
             optionList.Add("Proyectos Actuales");
 
-            List<string> descriptionText = new List<string>();
-            descriptionText.Add( "apply RESTful Service" );
-            List<string> membersList = new List<string>();
-            membersList.Add("member1");
-            membersList.Add("member2");
-            membersList.Add("member3");
-            List<string> oldMembersList = new List<string>();
-            oldMembersList.Add("oldMember1");
-            oldMembersList.Add("oldMember2");
-            oldMembersList.Add("oldMember3");
-            List<string> proyectsDone = new List<string> ();
-            proyectsDone.Add("apply another service here");
-            List<string> proyectsDoing = new List<string> ();
-            proyectsDoing.Add("apply yet another service");
+            //fill childs depends on the committee selected
+            if (s.Equals("committee1"))
+            {
+                descriptionText.Add("apply RESTful Service");
+                membersList.Add("member1");
+                membersList.Add("member2");
+                membersList.Add("member3");
+                oldMembersList.Add("oldMember1");
+                oldMembersList.Add("oldMember2");
+                oldMembersList.Add("oldMember3");
+                proyectsDone.Add("apply another service here");
+                proyectsDoing.Add("apply yet another service");
+            }
+
+            else if (s.Equals("committee2"))
+            {
+                descriptionText.Add("apply RESTful Service");
+                membersList.Add("member1");
+                membersList.Add("member2");
+                membersList.Add("member3");
+                oldMembersList.Add("oldMember1");
+                oldMembersList.Add("oldMember2");
+                oldMembersList.Add("oldMember3");
+                proyectsDone.Add("apply another service here");
+                proyectsDoing.Add("apply yet another service");
+            }
+
+            else if (s.Equals("committee8"))
+            {
+                descriptionText.Add("Promover la investigación y la estadarización de los " +
+                    "poyectos de la facultad, por medio de la calidad y su roboste con el fin " +
+                    "de lograr reconocimiento en la universidad como en colciencias");
+                membersList.Add("Carlos Delgado");
+                oldMembersList.Add("oldMember1");
+                proyectsDone.Add("apply another service here");
+                proyectsDoing.Add("apply yet another service");
+            }
+
+            else
+            {
+                descriptionText.Add("apply RESTful Service");
+                membersList.Add("member1");
+                membersList.Add("member2");
+                membersList.Add("member3");
+                oldMembersList.Add("oldMember1");
+                oldMembersList.Add("oldMember2");
+                oldMembersList.Add("oldMember3");
+                proyectsDone.Add("apply another service here");
+                proyectsDoing.Add("apply yet another service");
+            }
 
             //fill the dictionary with the options and its data
             optionsMapping.Add(optionList[0], descriptionText);
--- Committee_app_final/Resources/Resource.Designer.cs
+++ Committee_app_final/Resources/Resource.Designer.cs
@@ -99,62 +99,65 @@ namespace Committee_app_final
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050009
-			public const int buttonCommittee1 = 2131034121;
-			
-			// aapt resource value: 0x7f050012
-			public const int buttonCommittee10 = 2131034130;
-			
 			// aapt resource value: 0x7f05000a
-			public const int buttonCommittee2 = 2131034122;
+			public const int buttonCommittee1 = 2131034122;
+			
+			// aapt resource value: 0x7f050013
+			public const int buttonCommittee10 = 2131034131;
 			
 			// aapt resource value: 0x7f05000b
-			public const int buttonCommittee3 = 2131034123;
+			public const int buttonCommittee2 = 2131034123;
 			
 			// aapt resource value: 0x7f05000c
-			public const int buttonCommittee4 = 2131034124;
+			public const int buttonCommittee3 = 2131034124;
 			
 			// aapt resource value: 0x7f05000d
-			public const int buttonCommittee5 = 2131034125;
+			public const int buttonCommittee4 = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int buttonCommittee6 = 2131034126;
+			public const int buttonCommittee5 = 2131034126;
 			
 			// aapt resource value: 0x7f05000f
-			public const int buttonCommittee7 = 2131034127;
+			public const int buttonCommittee6 = 2131034127;
 			
 			// aapt resource value: 0x7f050010
-			public const int buttonCommittee8 = 2131034128;
+			public const int buttonCommittee7 = 2131034128;
 			
 			// aapt resource value: 0x7f050011
-			public const int buttonCommittee9 = 2131034129;
+			public const int buttonCommittee8 = 2131034129;
 			
-			// aapt resource value: 0x7f050001
-			public const int imageView1 = 2131034113;
-			
-			// aapt resource value: 0x7f050006
-			public const int lblListHeader = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int lblListItem = 2131034119;
-			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
-			
-			// aapt resource value: 0x7f050004
-			public const int linearLayout2 = 2131034116;
-			
-			// aapt resource value: 0x7f050003
-			public const int linearLayout3 = 2131034115;
-			
-			// aapt resource value: 0x7f050005
-			public const int lvExp = 2131034117;
-			
-			// aapt resource value: 0x7f050008
-			public const int mainText = 2131034120;
+			// aapt resource value: 0x7f050012
+			public const int buttonCommittee9 = 2131034130;
 			
 			// aapt resource value: 0x7f050002
-			public const int textView1 = 2131034114;
+			public const int imageView1 = 2131034114;
+			
+			// aapt resource value: 0x7f050007
+			public const int lblListHeader = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int lblListItem = 2131034120;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int linearLayout3 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int lvExp = 2131034118;
+			
+			// aapt resource value: 0x7f050009
+			public const int mainText = 2131034121;
+			
+			// aapt resource value: 0x7f050000
+			public const int maincommitteelayout = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
 			
 			static Id()
 			{
